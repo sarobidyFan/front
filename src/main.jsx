ece0c0cfb0f98fb0ab064fda26d3dce81b90e4b9
Sarobidy <hei.sarbidy.8@gmail.com>
2026-08-26 16:13:45 +0300

    chore: initialize project

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import App from "./App";
+import "./styles/main.css";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+            <App />
+    </StrictMode>
+);
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,9 @@
+import { Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => (
+    <Routes>
+        <Route path="/" element={<div>Hello, World!</div>} />
+    </Routes>
+);
+
+export default AppRoutes;
